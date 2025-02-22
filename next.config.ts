4aa2f4da36411deba3885aa308b20baf52a6f7d6
AdamsGeeky <muhdadamu0008@gmail.com>
2025-02-22 18:57:19 +0100

    feat: deplyment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "nacosgsu",
   output: "export",
   reactStrictMode: true,
-  distDir: "../public"
 };
 
 export default nextConfig;
